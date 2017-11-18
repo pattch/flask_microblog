f1131c8b3c591cc603b5cefc407fcb05a6328500
Samuel <innovatests@gmail.com>
2017-11-18 13:27:54 -0800

    Add SQLAlchemy Database Support, including Migrations.

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # Config File General Settings for the app
+import os
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'l337-53cr37'
 
@@ -8,3 +10,9 @@ OPENID_PROVIDERS = [
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ db_create.py
@@ -0,0 +1,13 @@
+# Creates an Empty Database Upon Which Migrations can be Performed
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+from app import db
+import os.path
+
+db.create_all()
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
