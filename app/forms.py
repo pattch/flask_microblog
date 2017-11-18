7022f35fee2c8a1dc89db900cfc792de9fe83033
Samuel <innovatests@gmail.com>
2017-11-18 00:11:48 -0800

    Finish step 3, adds support for forms and User Interface portion of logging in, OpenID.

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm as Form
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+# Create a Login Form class with 'fields' represented from wtforms module
+class LoginForm(Form):
+    # Form Validation on this field is that this is a Required Field
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 # Route DECORATORS -- Decorate the index function, adding Flask Functionality
 @app.route('/')
@@ -19,3 +20,15 @@ def index():
     return render_template('index.html.j2',
             user=user,
             posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        msg = 'Login Requested for OpenID=' + form.openid.data + ', Remember Me=' + str(form.remember_me.data)
+        flash(msg)
+        return redirect('/index')
+    return render_template('login.html.j2',
+                            title='Sign In',
+                            form=form,
+                            providers=app.config['OPENID_PROVIDERS'])
