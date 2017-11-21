7e642aff925bd0597ab3db9791f4901533941cf4
Samuel <innovatests@gmail.com>
2017-11-21 14:59:01 -0800

    Begin unit testing, remove unnecessary comments.

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+from app.models import Listing
+from app.views import *
+
+class SpaceBnBTestCase(unittest.TestCase):
+    def test_dummy_test(self):
+        assert True
+
+if __name__ == '__main__':
+    unittest.main()
