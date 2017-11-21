1b78e2b42a2c8e44ed34cc73190cb482014ed366
Samuel <innovatests@gmail.com>
2017-11-21 12:23:35 -0800

    Implement Listings Model and DB Migration, Begin Implementing Retrieval of All Listings

--- app/models.py
+++ app/models.py
@@ -1,44 +1,53 @@
 from app import db
 from hashlib import md5
 
-class User(db.Model):
+class Listing(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password = db.Column(db.String)
-    authenticated = db.Column(db.Boolean, default=False)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
-    about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime)
+    user = db.Column(db.String)
+    title = db.Column(db.String(140))
+    description = db.Column(db.String(1024))
+    expiration = db.Column(db.DateTime)
+    locationx = db.Column(db.Float)
+    locationy = db.Column(db.Float)
 
-    def __repr__(self):
-        return '<User %r>' % (self.nickname)
-
-    def is_active(self):
-        # All Users are Active Currently
-        return True
-
-    def get_id(self):
-        return str(self.id)
-
-    def is_authenticated(self):
-        return self.authenticated
-
-    def is_anonymous(self):
-        # No Current Support or Need for Anonymity
-        return False
-
-    # Gravatar Avatar, Uses md5 of Email and Pixel Size, Returns an Image
-    def avatar(self,size):
-        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), index=True)
-    body = db.Column(db.String(500))
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Post %r>' % (self.body)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     nickname = db.Column(db.String(64), index=True, unique=True)
+#     email = db.Column(db.String(120), index=True, unique=True)
+#     password = db.Column(db.String)
+#     authenticated = db.Column(db.Boolean, default=False)
+#     posts = db.relationship('Post', backref='author', lazy='dynamic')
+#     about_me = db.Column(db.String(140))
+#     last_seen = db.Column(db.DateTime)
+#
+#     def __repr__(self):
+#         return '<User %r>' % (self.nickname)
+#
+#     def is_active(self):
+#         # All Users are Active Currently
+#         return True
+#
+#     def get_id(self):
+#         return str(self.id)
+#
+#     def is_authenticated(self):
+#         return self.authenticated
+#
+#     def is_anonymous(self):
+#         # No Current Support or Need for Anonymity
+#         return False
+#
+#     # Gravatar Avatar, Uses md5 of Email and Pixel Size, Returns an Image
+#     def avatar(self,size):
+#         return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
+#
+#
+# class Post(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(120), index=True)
+#     body = db.Column(db.String(500))
+#     timestamp = db.Column(db.DateTime)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#
+#     def __repr__(self):
+#         return '<Post %r>' % (self.body)
--- app/views.py
+++ app/views.py
@@ -1,103 +1,129 @@
-from flask import render_template, flash, redirect, session, url_for, request, g
-from flask_login import login_user, logout_user, current_user, login_required
-from app import app, db, lm
+from flask import jsonify, request, abort
+from app import app, db
 from datetime import datetime
-from .forms import LoginForm, EditForm
-from .models import User
+from .models import Listing
 
-@lm.user_loader
-def user_loader(user_id):
-    # Given a User ID, return the correct User
-    return User.query.get(int(user_id))
 
-@app.before_request
-def before_request():
-    g.user = current_user
-    if g.user.is_authenticated:
-        g.user.last_seen = datetime.utcnow()
-        db.session.add(g.user)
-        db.session.commit()
-
-# Route DECORATORS -- Decorate the index function, adding Flask Functionality
-@app.route('/')
-@app.route('/index')
-@login_required
-def index():
-    # user = {'nickname': 'Miguel', 'id': 99999}
-    user = g.user
-    posts = [
-        {
-            'author': {'nickname': 'John', 'id':12345},
-            'body': 'Blog post by John Man'
-        },
-        {
-            'author': {'nickname': 'Susan Be Anthony', 'id': 67890},
-            'body': 'Coming out as Trans, call me Andy'
-        },
-        {
-            'author': {'nickname': 'Sam', 'id': 1},
-            'body': 'HARD CODED YO'
-        }
-    ]
-    return render_template('index.html.j2',
-            user=user,
-            posts=posts)
-
-@app.route('/login', methods=['GET','POST'])
-def login():
-    if g.user is not None and g.user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        remember_me = form.remember_me.data
-        # session['remember_me'] = remember_me
-        email = form.email.data
-        password = form.password.data
-        user = User.query.filter_by(email=email).first()
-
-        if user and password and user.password == password:
-            login_user(user, remember_me)
-            return redirect(request.args.get('next') or url_for('index'))
-        flash(unicode('Invalid Login Information.'))
-        return redirect(url_for('index'))
-    return render_template('login.html.j2',
-            title='Sign In',
-            form=form,
-            providers=app.config['OPENID_PROVIDERS'])
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-@app.route('/user/<nickname>')
-@login_required
-def user(nickname):
-    user = User.query.filter_by(nickname=nickname).first()
-    if not user:
-        flash(unicode('User %s not found.' % nickname))
-        redirect(url_for('index'))
-    posts = [
-        {'author': user, 'body': 'Test post 1'},
-        {'author': user, 'body': 'Test post 2'}
-    ]
-    return render_template('user.html.j2',
-            user=user,
-            posts=posts)
-
-@app.route('/edit', methods=['GET','POST'])
-@login_required
-def edit():
-    form = EditForm()
-    if form.validate_on_submit():
-        g.user.nickname = form.nickname.data
-        g.user.about_me = form.about_me.data
-        db.session.add(g.user)
-        db.session.commit()
-        flash(unicode('Changes Saved Successfully!'))
-        return redirect(url_for('edit'))
+@app.route('/api/listings/', methods=['GET','POST','DELETE'])
+def listings():
+    if request.method == 'GET':
+        return get_listings()
     else:
-        form.nickname.data = g.user.nickname
-        form.about_me.data = g.user.about_me
-    return render_template('edit.html.j2', form=form)
+        return 'STRANGENESS'
+    abort(404)
+
+# Convert a listing to a format that can be jsonified
+def jsonify_listing(listing):
+    return {
+        'id': listing.id,
+        'user': listing.user,
+        'title': listing.title,
+        'description': listing.description,
+        'location': {
+            'x': listing.locationx,
+            'y': listing.locationy
+        }
+    }
+
+# Return a list of all Listings in the DB
+def get_listings():
+    listings = Listing.query.all()
+    json_listings = jsonify([jsonify_listing(x) for x in listings])
+    return json_listings
+
+# @lm.user_loader
+# def user_loader(user_id):
+#     # Given a User ID, return the correct User
+#     return User.query.get(int(user_id))
+#
+# @app.before_request
+# def before_request():
+#     g.user = current_user
+#     if g.user.is_authenticated:
+#         g.user.last_seen = datetime.utcnow()
+#         db.session.add(g.user)
+#         db.session.commit()
+#
+# # Route DECORATORS -- Decorate the index function, adding Flask Functionality
+# @app.route('/')
+# @app.route('/index')
+# @login_required
+# def index():
+#     # user = {'nickname': 'Miguel', 'id': 99999}
+#     user = g.user
+#     posts = [
+#         {
+#             'author': {'nickname': 'John', 'id':12345},
+#             'body': 'Blog post by John Man'
+#         },
+#         {
+#             'author': {'nickname': 'Susan Be Anthony', 'id': 67890},
+#             'body': 'Coming out as Trans, call me Andy'
+#         },
+#         {
+#             'author': {'nickname': 'Sam', 'id': 1},
+#             'body': 'HARD CODED YO'
+#         }
+#     ]
+#     return render_template('index.html.j2',
+#             user=user,
+#             posts=posts)
+#
+# @app.route('/login', methods=['GET','POST'])
+# def login():
+#     if g.user is not None and g.user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         remember_me = form.remember_me.data
+#         # session['remember_me'] = remember_me
+#         email = form.email.data
+#         password = form.password.data
+#         user = User.query.filter_by(email=email).first()
+#
+#         if user and password and user.password == password:
+#             login_user(user, remember_me)
+#             return redirect(request.args.get('next') or url_for('index'))
+#         flash(unicode('Invalid Login Information.'))
+#         return redirect(url_for('index'))
+#     return render_template('login.html.j2',
+#             title='Sign In',
+#             form=form,
+#             providers=app.config['OPENID_PROVIDERS'])
+#
+# @app.route('/logout')
+# @login_required
+# def logout():
+#     logout_user()
+#     return redirect(url_for('index'))
+#
+# @app.route('/user/<nickname>')
+# @login_required
+# def user(nickname):
+#     user = User.query.filter_by(nickname=nickname).first()
+#     if not user:
+#         flash(unicode('User %s not found.' % nickname))
+#         redirect(url_for('index'))
+#     posts = [
+#         {'author': user, 'body': 'Test post 1'},
+#         {'author': user, 'body': 'Test post 2'}
+#     ]
+#     return render_template('user.html.j2',
+#             user=user,
+#             posts=posts)
+#
+# @app.route('/edit', methods=['GET','POST'])
+# @login_required
+# def edit():
+#     form = EditForm()
+#     if form.validate_on_submit():
+#         g.user.nickname = form.nickname.data
+#         g.user.about_me = form.about_me.data
+#         db.session.add(g.user)
+#         db.session.commit()
+#         flash(unicode('Changes Saved Successfully!'))
+#         return redirect(url_for('edit'))
+#     else:
+#         form.nickname.data = g.user.nickname
+#         form.about_me.data = g.user.about_me
+#     return render_template('edit.html.j2', form=form)
